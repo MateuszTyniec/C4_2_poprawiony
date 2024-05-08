312dcce4a9c71de634eabbc94b852518fbf5fda7
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-05-08 12:52:33 +0000

    C4_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,23 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
+      Scanner scanner = new 
+        Scanner(System.in);
+        System.out.print("Podaj imie: ");
+          String name = scanner.nextLine();
+          System.out.print("Podaj wiek: ");
+        int age = scanner.nextInt();
       s.addStudent(new Student("Krzysztof", 20));
       s.addStudent(new Student("Janusz", 40));
+      Student student = new Student(name,age);
+      s.addStudent(student);
+      
 
       var students = s.getStudents();
       for(Student current : students) {
